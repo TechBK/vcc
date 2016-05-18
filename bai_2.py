7dc0b9165186eb9ae40c7ed97887671e52fad17b
TechBK <quangbinh.nguyentrong@gmail.com>
2016-05-18 13:54:11 +0700

    fix check_cheo

--- bai_2.py
+++ bai_2.py
@@ -11,14 +11,14 @@ def check_cot(_ban_co, j):
     return sum(k) < 2
 
 def check_cheo(_ban_co, i, j, _i, _j):
+    k = [_ban_co[i][j]]
     while True:
         i += _i
         j += _j
         if i>=8 or j>=8 or i<0 or j<0:
             break
-        if _ban_co[i][j]:
-            return False
-    return True
+        k.append(_ban_co[i][j])
+    return sum(k)<2
 
 def Try(_ban_co):
     for i in range(0, 8):
@@ -28,35 +28,43 @@ def Try(_ban_co):
         if not check_cot(_ban_co, j):
             return False
     for i in range(0, 8):
-        for j in range(0, 8):
-            if (not check_cheo(_ban_co, i, j, 1, 1)) \
-                and (not check_cheo(_ban_co, i, j, 1, -1))\
-                and (not check_cheo(_ban_co, i, j, -1, 1))\
-                and (not check_cheo(_ban_co, i, j, -1, -1)):
-                return False
-            else:
-                return True
+        if not check_cheo(_ban_co, i, 0, 1, 1):
+            return False
 
-def xep_co(__ban_co, i, j):
+    for i in range(0, 8):
+        if not check_cheo(_ban_co, i, 7, -1, 1):
+            return False
+    for j in range(0, 8):
+        if not check_cheo(_ban_co, 0, j, 1, 1):
+            return False
+
+    for j in range(0, 8):
+        if not check_cheo(_ban_co, 0, j, -1, 1):
+            return False
+    return True
+
+def xep_co(__ban_co, i):
     print("Buoc", i)
     for line in __ban_co:
         print(line)
-    _ban_co = []
-    for line in __ban_co:
-        _ban_co.append(line[:])
 
-    if i == 8 and j == 8:
+
+
+    if i == 8:
         print('ket qua')
-        for line in _ban_co:
+        for line in __ban_co:
             print(line)
         return True
-    _ban_co[i][j] = 1
 
-    if Try(_ban_co):
-        if xep_co(_ban_co, i + 1, j + 1):
-            return True
-        else:
-            return False
+    if Try(__ban_co):
+        for j in range(0, 8):
+            _ban_co = []
+            for line in __ban_co:
+                _ban_co.append(line[:])
+            _ban_co[i][j] = 1
+            if xep_co(_ban_co, i+1):
+                return True
+
     else:
         return False
 
@@ -64,4 +72,4 @@ def xep_co(__ban_co, i, j):
 ban_co = []
 for i in range(0, 8):
     ban_co.append([0,0,0,0,0,0,0,0])
-xep_co(ban_co,0, 0)
+xep_co(ban_co,0)
